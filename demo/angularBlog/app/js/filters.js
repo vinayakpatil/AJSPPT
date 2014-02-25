b3bc3490346ff23dd843ff5d94b478f954c52cd9
Vinayak Patil <vinayakpat@cybage.com>
2014-02-25 19:19:29 +0530

    Updates application with form validations

--- demo/angularBlog/app/js/filters.js
+++ demo/angularBlog/app/js/filters.js
@@ -3,8 +3,20 @@
 /* Filters */
 
 angular.module('myApp.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
+filter('interpolate', ['version',
+    function(version) {
+        return function(text) {
+            return String(text).replace(/\%VERSION\%/mg, version);
+        }
     }
-  }]);
+]).
+filter('blogTime', function() {
+    return function(date){
+        var days, dateDiff;
+        dateDiff = new Date() - new Date(date);
+        days = dateDiff/(60 * 60 * 24 * 1000);
+        if(Math.floor(days)>30){
+            return Math.floor(days/31) + ' Months ago';
+        }
+    }
+});
